0d6e6d4d4edae8bd918218a20fb66d9d914b796f
Ryan Manchester <ryanm17@gmail.com>
2022-01-14 14:41:22 -0600

    tests required ERC20 attributes

--- test/Token.test.js
+++ test/Token.test.js
@@ -2,11 +2,32 @@ const Token = artifacts.require('./Token')
 require('chai').use(require('chai-as-promised')).should()
 
 contract('Token', (accounts) => {
+  let token
+  const name = "DApp Token"
+  const symbol = "DApp"
+  const decimals = '18'
+  const totalSupply = '1000000000000000000000000'
+
+  beforeEach(async () => {
+    token = await Token.new()
+  })
+
   describe('deployment', () => {
     it('tracks the name', async () => {
-      const token = await Token.new()
       const result = await token.name()
-      result.should.equal('My Name')
+      result.should.equal(name)
+    })
+    it('tracks the symbol', async () => {
+      const result = await token.symbol()
+      result.should.equal(symbol)
+    })
+    it('tracks the decimals', async () => {
+      const result = await token.decimals()
+      result.toString().should.equal(decimals)
+    })
+    it('tracks the total supply', async () => {
+      const result = await token.totalSupply()
+      result.toString().should.equal(totalSupply)
     })
   })
 })
