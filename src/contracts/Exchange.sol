a5feae42b21bed0032197b452d2310a7f9bd1223
Ryan Manchester <ryanm17@gmail.com>
2022-02-16 22:46:36 -0500

    imports SafeMath

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,14 +1,19 @@
 pragma solidity^0.5.0;
 import './Token.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+
 
 contract Exchange {
+  using SafeMath for uint256;
   address public feeAccount;
   uint256 public feePercent;
+  mapping(address => mapping(address => uint256)) public tokens;
   constructor (address _feeAccount, uint256 _feePercent) public {
     feeAccount = _feeAccount;
     feePercent = _feePercent;
   }
   function depositToken(address _token, uint _amount) public {
-    Token(_token).transferFrom(msg.sender, address(this), _amount);
+    require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+    tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
   }
 }
