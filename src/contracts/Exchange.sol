f676e9240b6ea61f2cb72b54a566aee859d91f85
Ryan Manchester <ryanm17@gmail.com>
2022-02-18 12:52:58 -0500

    tracks order count

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -10,14 +10,34 @@ contract Exchange {
   uint256 public feePercent;
   address constant ETHER = address(0); //store Ether in tokens mapping with blank address
   mapping(address => mapping(address => uint256)) public tokens;
+  mapping(uint256 => _Order) public orders;
+  uint256 public orderCount;
 
   event Deposit(address token, address user, uint256 amount, uint256 balance);
-  event Withdraw(address token, address user, uint amount, uint balance);
+  event Withdraw(address token, address user, uint256 amount, uint256 balance);
+  event Order(
+    uint id,
+    address user,
+    address tokenGet,
+    uint amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+
+  struct _Order {
+    uint id;
+    address user;
+    address tokenGet;
+    uint amountGet;
+    address tokenGive;
+    uint256 amountGive;
+    uint256 timestamp;
+  }
 
   //model order
   //store order
   //add order to storage
-  
 
   constructor (address _feeAccount, uint256 _feePercent) public {
     feeAccount = _feeAccount;
@@ -32,14 +52,14 @@ contract Exchange {
     emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
   }
 
-  function withdrawEther(uint _amount) public {
+  function withdrawEther(uint256 _amount) public {
     require(tokens[ETHER][msg.sender] >= _amount);
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
     msg.sender.transfer(_amount);
     emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
   }
 
-  function depositToken(address _token, uint _amount) public {
+  function depositToken(address _token, uint256 _amount) public {
     require(_token != ETHER);
     require(Token(_token).transferFrom(msg.sender, address(this), _amount));
     tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
@@ -56,5 +76,11 @@ contract Exchange {
 
   function balanceOf(address _token, address _user) public view returns (uint256) {
     return tokens[_token][_user];
+  }
+
+  function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
   }
 }
