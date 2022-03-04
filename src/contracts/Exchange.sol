498652e5fce55bc2e90b9e6f69c2c93766693bcd
Ryan Manchester <ryanm17@gmail.com>
2022-03-04 12:56:33 -0500

    begins internal trade function

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -36,6 +36,17 @@ contract Exchange {
     uint256 timestamp
   );
 
+  event Trade(
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    address userFill,
+    uint256 timestamp
+  );
+
   struct _Order {
     uint256 id;
     address user;
@@ -100,5 +111,25 @@ contract Exchange {
     require(_order.id == _id);
     orderCancelled[_id] = true;
     emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+  }
+
+  function fillOrder(uint256 _id) public {
+    //Fetch order
+    //Mark order as filled
+    _Order storage _order = orders[id];
+    _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+  }
+  function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+    uint256 _feeAmount = _amountGive.mul(feePercent).div(100);
+    //Execute order
+      //charge fees
+    tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+    tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+    tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+    tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+    tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+    //emit Trade event
+    emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
+
   }
 }
